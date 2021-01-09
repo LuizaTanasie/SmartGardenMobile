b538bd769703f6ad1562e27dcd2ee0099eb5ddca
Luiza Tanasie <luiza.tanasie@qubiz.com>
2021-01-09 19:44:03 +0200

    UI

---
+++ SmartGardenMobile/SmartGardenMobile/Helpers/MathConverter.cs
@@ -0,0 +1,32 @@
+﻿using SimpleExpressionEvaluator;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SmartGardenMobile.Helpers
+{
+    public class MathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            // Parse value into equation and remove spaces
+            var mathEquation = parameter as string;
+            mathEquation = mathEquation.Replace(" ", "");
+            mathEquation = mathEquation.Replace("@VALUE", value.ToString());
+            ExpressionEvaluator engine = new ExpressionEvaluator();
+            return engine.Evaluate(mathEquation);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
